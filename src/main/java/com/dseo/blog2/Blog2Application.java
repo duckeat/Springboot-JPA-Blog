32bbc39127c316ce35dc99f847b7509ec590bdf6
[서비스플랫폼]서상덕 <dseo@utplus.co.kr>
2022-11-28 00:16:41 +0900

    환경 세팅 완료

---
+++ src/main/java/com/dseo/blog2/Blog2Application.java
@@ -0,0 +1,13 @@
+package com.dseo.blog2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog2Application.class, args);
+	}
+
+}
